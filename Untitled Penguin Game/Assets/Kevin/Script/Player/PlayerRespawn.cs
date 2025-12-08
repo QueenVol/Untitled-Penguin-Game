dc34b21d42daf5def0ea428242f6203d904bc1f5
QueenVol <159581003+QueenVol@users.noreply.github.com>
2025-12-07 23:53:25 -0500

    Save Location Updated

---
+++ Untitled Penguin Game/Assets/Kevin/Script/Player/PlayerRespawn.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+    [Header("Fixed Spawn Point")]
+    public Transform spawnPoint;
+
+    private Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Respawn()
+    {
+        if (spawnPoint == null)
+        {
+            return;
+        }
+
+        transform.position = spawnPoint.position;
+
+        if (rb != null)
+        {
+            rb.velocity = Vector2.zero;
+            rb.angularVelocity = 0f;
+        }
+    }
+}
