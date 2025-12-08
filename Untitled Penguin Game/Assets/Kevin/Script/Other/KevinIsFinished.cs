17874bd65547434530dd0710ca731fe97720b9a7
QueenVol <159581003+QueenVol@users.noreply.github.com>
2025-12-08 02:56:39 -0500

    Change

--- Untitled Penguin Game/Assets/Kevin/Script/Other/KevinIsFinished.cs
+++ Untitled Penguin Game/Assets/Kevin/Script/Other/KevinIsFinished.cs
@@ -6,10 +6,27 @@ public class KevinIsFinished : MonoBehaviour
 {
     public static bool kevinIsFinished = false;
 
-    public void TheEnd()
-    {
-        EggSpawner spawner = FindObjectOfType<EggSpawner>();
+    public float delayTime = 2f;
 
+    private bool hasTriggered = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (hasTriggered) return;
+
+        if (other.CompareTag("Player"))
+        {
+            hasTriggered = true;
+
+            StartCoroutine(DelayTriggerEnd());
+        }
+    }
+
+    IEnumerator DelayTriggerEnd()
+    {
+        yield return new WaitForSeconds(delayTime);
+        kevinIsFinished = true;
+        EggSpawner spawner = FindObjectOfType<EggSpawner>();
         if (spawner != null)
         {
             spawner.YKYVersionLoadRandomSceneBasedOnBools();
